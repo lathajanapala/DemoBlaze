178410cf9c1bb04c161b809bace70cc0e74bed01
lathajanapala <pushpalatha7305@gmail.com>
2026-01-15 15:07:14 -0600

    changed assertions

--- tests/mouseHove.spec.ts
+++ tests/mouseHove.spec.ts
@@ -1,8 +1,8 @@
 import{test,expect} from '@playwright/test'
 test("Handle Mouse Hover and click one of the link",async ({page})=>{
   await page.goto("https://testautomationpractice.blogspot.com/");
-  page.getByRole('button',{name: "Point Me"}).hover()
+  await page.getByRole('button',{name: "Point Me"}).hover()
   const hoverLinks =page.locator(".dropdown-content a:visible");
-  expect(hoverLinks).toBe(2);
+  await expect(hoverLinks).toHaveCount(2);
   await hoverLinks.filter({hasText:'Mobiles'}).click();
 })
